372a528595c7fa50d12958109f2b26a4f828a920
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-10-10 09:16:38 -0400

    wrote methods to both delete/add information to object destructively/nondestructively

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+const driver = {};
+
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+  const newDriver = {...driver}
+  newDriver[key] = value;
+  
+  return newDriver
+}
+
+      
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+  const newDriver = {}
+  newDriver[key] = value
+  Object.assign(driver, newDriver)
+  return driver
+}
+
+
+const deleteFromDriverByKey = (driver, key) => {
+  const newDriver = {...driver};
+  delete newDriver[key];
+  return newDriver
+}
+
+
+const destructivelyDeleteFromDriverByKey = (driver, key) => {
+  delete driver[key];
+  return driver
+}
+
+destructivelyUpdateDriverWithKeyAndValue(driver, "hi", "there")
+updateDriverWithKeyAndValue(driver, "hi", "there")
